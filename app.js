4ace63aa8f46b81a51ae9c71755c9cc167ef2163
aruay99 <aruaiberdk@gmail.com>
2022-01-28 01:40:51 +0200

    fix bug with game.js being saved in the wrong folder

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import P5 from 'p5';
-import game from './scripts/sketches/game';
+import game from './scripts/game';
 // BUG: zooming messes the game up, allows you to cheat and look ahead. potential fix: render only what's on screen (100% of it)
 const scene = new P5(game);
 console.log(scene);
